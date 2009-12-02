90640d83872d0c4986e157e2fb0ff3871e98eb09
Breck Yunits <breck7@gmail.com>
2009-12-01 22:36:58 -0800

    added editing, deleting, and more

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ class Blog {
 	{
 		include("posts.php");
 		$this->posts = $posts;
+		arsort($this->posts);
 		foreach ($posts as $key => $array)
 		{
 			$this->titles[strtolower(str_replace(" ","_",preg_replace('/[^a-z0-9 ]/i',"",$array['Title'])))] = $key;
@@ -20,21 +21,29 @@ class Blog {
 			if (!isset($_GET['post'])) // create new post
 			{
 				$this->posts[time()] = array("Title" => $_POST['title'], "Essay" => $_POST['essay']);
-				file_put_contents("posts.php", "<?php \$posts= ".var_export($this->posts, true) . "?>");
 			}
-			else { // edit posts
-			
+			elseif (isset($this->posts[$_GET['post']]) && isset($_POST['delete'])) // edit posts
+			{
+				unset($this->posts[$_GET['post']]);
 			}
+			elseif (isset($this->posts[$_GET['post']])) // edit posts
+			{
+				$this->posts[$_GET['post']] = array("Title" => $_POST['title'], "Essay" => $_POST['essay']);
+			}
+			file_put_contents("posts.php", "<?php \$posts= ".var_export($this->posts, true) . "?>");
+			arsort($this->posts);
 		}
 	}
 	public function displayEditor ()
 	{
 		$title = "";
 		$essay = "";
+		$delete = "";
 		if (isset($_GET['post']) && isset($this->posts[$_GET['post']]))
 		{
 			$title = $this->posts[$_GET['post']]['Title'];
 			$essay = $this->posts[$_GET['post']]['Essay'];
+			$delete = "<input type=\"submit\" value=\"Delete\" onclick=\"return confirm('DELETE. Are you sure?');\">";
 		}
 		$content = <<<LONG
 		<form method="post" action="">
@@ -42,7 +51,7 @@ class Blog {
 		<tr><td>Title</td><td><input type="text" name="title" size="25" value="$title"></td></tr>
 		<tr><td>Content</td><td><textarea name="essay" rows="30" cols="80">$essay</textarea></td></tr>
 		<tr><td>Password</td><td><input type="password" name="password"></td></tr>
-		<tr><td></td><td><input type="submit" value="Save"></td></tr></table>
+		<tr><td></td><td><input type="submit" value="Save">$delete</td></tr></table>
 		</form>
 		Edit a Post:<br>
 LONG;
@@ -64,13 +73,13 @@ LONG;
 		{
 			$post = $this->posts[$this->titles[substr($_GET['r'],1)]];
 			$this->displayPage($post['Title'],substr($post['Essay'],0,100),
-			"<h1>{$post['Title']}</h1><div>{$post['Essay']}<br><br>".date("m/d/Y")."</div>");
+			"<h1>{$post['Title']}</h1><div>".nl2br($post['Essay'])."<br><br>".date("m/d/Y")."</div>");
 		}
 		else { // Homepage
 			$last_five = "";
 			foreach ($this->posts as $post)
 			{
-				$last_five .= "<h1><a href=\"".strtolower(str_replace(" ","_",preg_replace('/[^a-z0-9 ]/i',"",$post['Title'])))."\">{$post['Title']}</a></h1><div>{$post['Essay']}<br><br>".date("m/d/Y")."</div><br><br>";
+				$last_five .= "<h1><a href=\"".strtolower(str_replace(" ","_",preg_replace('/[^a-z0-9 ]/i',"",$post['Title'])))."\">{$post['Title']}</a></h1><div>".nl2br($post['Essay'])."<br><br>".date("m/d/Y")."</div><br><br>";
 			}
 			$this->displayPage($this->blog_title, $this->blog_description,
 			$last_five); 
@@ -81,30 +90,20 @@ LONG;
 		?>
 			<html>
 			<head>
-			<style>
-			body {
-				font-family: Georgia;
-				color: #888888;
-			}
-			h1 {
-				margin-top: 0px;
-			}
-			#content {
-				float: left;
-				
-			}
-			#sidebar {
-				float: right;
-			}
+			<style type="text/css">
+			body {font-family: Georgia; color: #888888;}
+			h1 {margin-top: 0px;}
+			#content {float: left;}
+			#sidebar {float: right;}
 			</style>
 			<title><?php echo $title;?></title>
 			<meta name="description" content="<?php echo $description;?>">
 			</head>
 			<body>
 			<div id="content">
-			<?php
-			echo $body;
-			?>
+				<?php
+					echo $body;
+				?>
 			</div>
 			<div id="sidebar">
 				<a href="/" style="text-decoration:none;"><?php echo $this->blog_title;?></a><br><br>
