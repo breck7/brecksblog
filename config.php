dc32deb5ae263937f103958bfa662a6cd893f8f9
Breck Yunits <breck7@gmail.com>
2009-12-09 15:35:03 -0800

    put settings into config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+define("BLOG_PASSWORD","changeme");
+define("BLOG_TITLE","My Blog");
+define("BLOG_DESCRIPTION","My weblog where I write my thoughts.");
+
+function blog_analytics()
+{
+	?>
+	<!--Put your Google Analytics or other analytics code here -->
+	<?php
+}
+?>
