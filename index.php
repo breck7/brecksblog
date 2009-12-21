c3c6c18e2e8b1221dafb661a8c10722a9f384ab8
Breck Yunits <breck7@gmail.com>
2009-12-21 09:26:50 -0800

    tweaked theme

--- index.php
+++ index.php
@@ -103,7 +103,8 @@ LONG;
 			<head>
 			<style type="text/css">
 			body {font-family: arial; color: #222222; padding: 20px;}
-			h1 {margin-top: 0px;}
+h1 {margin-top: 0px; border-bottom: 1px solid #999999;}
+                        h1 a{text-decoration:none; color: #0000AA;font-size:.8em;}
 			/* theme inspired by http://crypto.stanford.edu/~blynn/c/index.html */
 			#sidebar {font-size:.8em;background:#F9F9F9;
 			margin-left: 40px;padding: 8px;}
